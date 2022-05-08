4d72106aece8f307852c4b17f4f203ccf8112a6f
Josh Braden <josbraden@gmail.com>
2022-05-08 15:42:53 -0500

    Basic structure

--- ercotscraper.py
+++ ercotscraper.py
@@ -1,3 +1,58 @@
 # Ercot Scraper
 # @author Josh Braden
 # Python script to download report files from Ercot and insert them into a MySQL database for Grafana dashboards
+
+# Imports
+import sys
+import mysql.connector
+
+# Variables
+version = "v0.1"
+# DB Stuff
+dbhost = "127.0.0.1"
+dbuser = "dbuser"
+dbpasswd = "password"
+dbschema = "ercot"
+dbcompress = False
+# Helptext
+helptext = "Usage: ercotscraper.py foo"
+
+
+# Function to test database connection
+def testMySQL():
+    print("Testing database connection...", end='')
+    try:
+        connection = mysql.connector.connect(
+            host=dbhost, user=dbuser, passwd=dbpasswd,
+            database=dbschema, compress=dbcompress)
+
+    except mysql.connector.Error as err:
+        print(err)
+        return -1
+
+    else:
+        connection.set_charset_collation(dbcharset, dbcollation)
+        connectioncursor = connection.cursor()
+        connectioncursor.execute("SELECT COUNT(*) FROM messageGrabs")
+        connresult = connectioncursor.fetchone()
+        for i in connresult:
+            print("Success")
+
+        connectioncursor.close()
+        connection.close()
+        return 0
+
+
+# Execution start
+print("Starting kingwood scraper " + version)
+if testMySQL() != 0:
+    sys.exit(1)
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "help" or sys.argv[1] == "-h":
+        # Print helptext
+        print(helptext)
+
+else:
+    # Continue
+    print("foo")
