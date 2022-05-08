5f40219937006af48b89c0f0cb7a132835793260
Josh Braden <josbraden@gmail.com>
2022-05-08 15:46:59 -0500

    Initialize db schema: Only includes download files and solar

---
+++ ercot.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 08, 2022 at 03:46 PM
+-- Server version: 10.3.34-MariaDB-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ercot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `downloads`
+--
+
+CREATE TABLE `downloads` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ercot_report_id` int(11) NOT NULL,
+  `ercot_doc_id` bigint(11) UNSIGNED NOT NULL,
+  `downloaded` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `solar`
+--
+
+CREATE TABLE `solar` (
+  `id` bigint(11) UNSIGNED NOT NULL,
+  `ercot_doc_id` bigint(11) UNSIGNED NOT NULL,
+  `SYSTEM_WIDE` float NOT NULL COMMENT 'Solar power generated system wide in MW',
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Holds 5-minute interval system-wide solar generation';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `downloads`
+--
+ALTER TABLE `downloads`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ercot_doc_id` (`ercot_doc_id`);
+
+--
+-- Indexes for table `solar`
+--
+ALTER TABLE `solar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `downloads`
+--
+ALTER TABLE `downloads`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `solar`
+--
+ALTER TABLE `solar`
+  MODIFY `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
