78e47ff93027602585f6268c55f8717f21e3bd4f
Josh Braden <josbraden@gmail.com>
2022-05-09 13:48:00 -0500

    Port checkDbExistence()

--- mysql.py
+++ mysql.py
@@ -38,3 +38,36 @@ def testMySQL():
         connectioncursor.close()
         connection.close()
         return 0
+
+
+# Function to get database existence for a given ercot_doc_id
+# Returns 1 if true, 0 if false
+def checkDbExistence(ercot_doc_id, table):
+    query = "SELECT COUNT(*) FROM "
+    query += table
+    query += " WHERE ercot_doc_id = "
+    query += str(ercot_doc_id)
+    try:
+        connection = mysql.connector.connect(
+            host=dbhost, user=dbuser, passwd=dbpasswd,
+            database=dbschema, compress=dbcompress)
+
+    except mysql.connector.Error as err:
+        print(err)
+        return -1
+
+    else:
+        connection.set_charset_collation(dbcharset, dbcollation)
+        connectioncursor = connection.cursor()
+        connectioncursor.execute(query)
+        connresult = connectioncursor.fetchone()
+        if connresult[0] == 1:
+            ret = 1
+        elif connresult[0] == 0:
+            ret = 0
+        else:
+            ret = -1
+
+        connectioncursor.close()
+        connection.close()
+        return ret
