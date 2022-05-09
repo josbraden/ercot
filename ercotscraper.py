325dc48f31e90566597d9cf1ea3976f2d7c80e96
Josh Braden <josbraden@gmail.com>
2022-05-09 12:48:11 -0500

    Move DB variables, imports, and test function to new sql file

--- ercotscraper.py
+++ ercotscraper.py
@@ -5,8 +5,8 @@
 # Imports
 import sys
 import requests
-import mysql.connector
 from bs4 import BeautifulSoup
+from mysql import *
 
 # Variables
 version = "v0.0"
@@ -14,42 +14,8 @@ version = "v0.0"
 baseUrl = "http://mis.ercot.com"
 reportBaseUrl = "/misapp/GetReports.do?reportTypeId="
 docBaseUrl = "/misdownload/servlets/mirDownload?mimic_duns=000000000&doclookupId="
-# DB Stuff
-dbhost = "127.0.0.1"
-dbuser = "dbuser"
-dbpasswd = "password"
-dbschema = "ercot"
-dbcompress = False
-# These should be fine unless you also altered the schema file
-dbcharset = "utf8mb4"
-dbcollation = "utf8mb4_general_ci"
 # Helptext
 helptext = "Usage: ercotscraper.py foo"
-
-
-# Function to test database connection
-def testMySQL():
-    print("Testing database connection...", end='')
-    try:
-        connection = mysql.connector.connect(
-            host=dbhost, user=dbuser, passwd=dbpasswd,
-            database=dbschema, compress=dbcompress)
-
-    except mysql.connector.Error as err:
-        print(err)
-        return -1
-
-    else:
-        connection.set_charset_collation(dbcharset, dbcollation)
-        connectioncursor = connection.cursor()
-        connectioncursor.execute("SELECT COUNT(*) FROM downloads")
-        connresult = connectioncursor.fetchone()
-        for i in connresult:
-            print("Success")
-
-        connectioncursor.close()
-        connection.close()
-        return 0
 
 
 # Function to get the list of available documents for a given report
--- mysql.py
+++ mysql.py
@@ -1,2 +1,40 @@
 # MySQL functions for the ercot scraper
 # @author Josh Braden
+
+# Imports
+import mysql.connector
+
+# DB Variables
+dbhost = "127.0.0.1"
+dbuser = "dbuser"
+dbpasswd = "password"
+dbschema = "ercot"
+dbcompress = False
+# These should be fine unless you also altered the schema file
+dbcharset = "utf8mb4"
+dbcollation = "utf8mb4_general_ci"
+
+
+# Function to test database connection
+def testMySQL():
+    print("Testing database connection...", end='')
+    try:
+        connection = mysql.connector.connect(
+            host=dbhost, user=dbuser, passwd=dbpasswd,
+            database=dbschema, compress=dbcompress)
+
+    except mysql.connector.Error as err:
+        print(err)
+        return -1
+
+    else:
+        connection.set_charset_collation(dbcharset, dbcollation)
+        connectioncursor = connection.cursor()
+        connectioncursor.execute("SELECT COUNT(*) FROM downloads")
+        connresult = connectioncursor.fetchone()
+        for i in connresult:
+            print("Success")
+
+        connectioncursor.close()
+        connection.close()
+        return 0
