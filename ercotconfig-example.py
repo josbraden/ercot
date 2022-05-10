f1d7fc26d3fcaa4f672438f73f322eb10b359a18
Josh Braden <josbraden@gmail.com>
2022-05-10 12:05:50 -0500

    Inital config file and gitignore update

---
+++ ercotconfig-example.py
@@ -0,0 +1,12 @@
+# Example config file
+# Copy this to ercotconfig.py and change values for your environment
+
+# DB Variables
+dbhost = "127.0.0.1"
+dbuser = "dbuser"
+dbpasswd = "password"
+dbschema = "ercot"
+dbcompress = False
+# These should be fine unless you also altered the schema file
+dbcharset = "utf8mb4"
+dbcollation = "utf8mb4_general_ci"
