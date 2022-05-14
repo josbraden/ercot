c7443a7a8a9fe655bcab3f38c18ec93ded760324
Josh Braden <josbraden@gmail.com>
2022-05-14 12:31:51 -0500

    Only get average price

--- ercotscraper.py
+++ ercotscraper.py
@@ -334,6 +334,8 @@ def report_dctieflows():
 # Similar to the DC tie flow table, this should be two tables.
 def report_prices():
     ercot_report_id = 12301
+    # There's a lot of extra data in this set, only get the average for now.
+    allowedPrices = ["HB_BUSAVG"]
     if verbose:
         print("Running prices")
 
@@ -357,17 +359,18 @@ def report_prices():
 
         fp.close()
         for i in range(1, (len(csvData) - 1)):
-            queryData = ""
-            # The datetime is a little weird, instead of messing with minutes/seconds I'm just rounding to the hour. Close enough.
-            # Also, sometimes there's a 24 instead of 0 for midnight I guess
-            tempHour = str(csvData[i][1])
-            if tempHour == "24":
-                tempHour = "0"
+            if str(csvData[i][3]) in allowedPrices:
+                queryData = ""
+                # The datetime is a little weird, instead of messing with minutes/seconds I'm just rounding to the hour. Close enough.
+                # Also, sometimes there's a 24 instead of 0 for midnight I guess
+                tempHour = str(csvData[i][1])
+                if tempHour == "24":
+                    tempHour = "0"
 
-            tempDateTime = str(csvData[i][0]) + " " + tempHour + ":00:00"
-            sqlDateTime = datetime.datetime.strptime(tempDateTime, '%m/%d/%Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
-            queryData = "'" + str(csvData[i][3]) + "','" + str(csvData[i][4]) + "'," + str(csvData[i][5]) + ",'" + sqlDateTime + "'"
-            insertPrices(queryData)
+                tempDateTime = str(csvData[i][0]) + " " + tempHour + ":00:00"
+                sqlDateTime = datetime.datetime.strptime(tempDateTime, '%m/%d/%Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+                queryData = "'" + str(csvData[i][3]) + "','" + str(csvData[i][4]) + "'," + str(csvData[i][5]) + ",'" + sqlDateTime + "'"
+                insertPrices(queryData)
 
         os.remove(fullFilename)
 
